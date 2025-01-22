7a90254e36d67f7160da7859dbd7570d56a6db6d
Strukov <strukov.eugen@gmail.com>
2025-01-22 06:21:51 +0100

    Add laravel-project as part of the main repository

---
+++ laravel-project/app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+use App\Filament\Auth\Login;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->spa()
+            ->path('admin')
+            ->login(Login::class)
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            //->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                Widgets\FilamentInfoWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ])
+            ;
+    }
+
+    /*
+
+    EncryptCookies::class,
+    AddQueuedCookiesToResponse::class,
+    StartSession::class,
+    AuthenticateSession::class,
+    ShareErrorsFromSession::class,
+    VerifyCsrfToken::class,
+    SubstituteBindings::class,
+    DisableBladeIconComponents::class,
+    DispatchServingFilamentEvent::class,
+
+
+    public function boot(): void
+    {
+        Filament::serving(function () {
+            Filament::registerNavigationItems([
+                // Навигация для Dashboard
+                NavigationItem::make('Dashboard')
+                    ->url(route('filament.admin.pages.dashboard', ['wire:navigate' => true]))
+                    ->icon('heroicon-o-home')
+                    ->group('Main Navigation'),
+
+                NavigationItem::make('Users')
+                    ->url(route('filament.admin.resources.users.index', ['wire:navigate' => true]))
+                    ->icon('heroicon-o-users')
+                    ->group('Main Navigation')
+                    ->badge(fn () => \App\Models\User::count())
+            ]);
+        });
+    }*/
+
+    /*
+    public function boot()
+    {
+        Filament::serving(function () {
+            Filament::registerPages([
+                Login::class,
+            ]);
+        });
+    } */
+
+
+}
